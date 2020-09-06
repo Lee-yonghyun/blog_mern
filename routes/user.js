2fa7e12c9f6a25b5f01973dd4055d5100b75a965
이용현 <dydwk231@naver.com>
2020-09-06 20:53:36 +0900

    signup user data #1

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,46 @@ const userModel = require('../models/user')
 
 
 
+// @route   POST http://localhost:6000/users/registor
+// @desc    Registor user
+// @access  Public
+
+router.post('/registor',(req,res) => {
+
+    const { name, email, password } = req.body
+
+    const newUser = new userModel({
+        name, email, password
+    })
+
+    newUser
+        .save()
+        .then(user => {
+            res.json({
+                message:'saved userdata',
+                userInfo: user
+            })
+        })
+        .catch(err => {
+            res.json({
+                message:err.message
+            })
+        })
+
+})
+
+
+
+
+// @route   POST http://localhost:6000/users/login
+// @desc    login user & get token
+// @access  Public
+
+router.post('/login',(req,res) => {
+
+
+})
+
+
 
 module.exports = router
