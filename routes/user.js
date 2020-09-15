f7a369711872b0ca0feea146433506e267f808e1
이용현 <dydwk231@naver.com>
2020-09-15 23:48:18 +0900

    better user signup, return jwt

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const userModel = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 
@@ -12,24 +14,57 @@ router.post('/registor',(req,res) => {
 
     const { name, email, password } = req.body
 
-    const newUser = new userModel({
-        name, email, password
-    })
 
-    newUser
-        .save()
+    userModel
+        .findOne({email})
         .then(user => {
-            res.json({
-                message:'saved userdata',
-                userInfo: user
-            })
+            console.log(user)
+
+            if (user) {
+                return res.json({
+                    message:"email already existed"
+                })
+            }
+
+            else{
+
+                bcrypt.hash(password, 10, (err,hash) => {
+
+                    if (err) {
+                        return res.json({
+                            message:err.message
+                        })
+                    }
+
+                    else {
+
+                        const newUser = new userModel({
+                            name, email, password:hash
+                        })
+
+                        newUser
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    message:'saved data',
+                                    userInfo:user
+                                })
+                            })
+                            .catch(err => {
+                                res.json({
+                                    message:err.message
+                                })
+                            })
+                    }
+                })
+            }
+
         })
         .catch(err => {
             res.json({
                 message:err.message
             })
         })
-
 })
 
 
@@ -40,6 +75,50 @@ router.post('/registor',(req,res) => {
 // @access  Public
 
 router.post('/login',(req,res) => {
+
+    const {name , email, password} = req.body
+
+    userModel
+        .findOne({email})
+        .then(user => {
+
+            if (!user) {
+                return res.json({
+                    message:'your email wrong'
+                })
+            }
+
+            else{
+
+                bcrypt.compare(password, user.password, (err,result) => {
+
+                  if (err || result === false) {
+                      return res.json({
+                          success:result,
+                          message:'password incorrect'
+                      })
+                  }
+                  else {
+                      const token = jwt.sign(
+                          {email:user.email, userId:user._id},
+                          "key",
+                          {expiresIn:"1d"}
+                      )
+
+                      res.json({
+                          success:result,
+                          token:token
+                      })
+                  }
+                })
+            }
+
+        })
+        .catch(err => {
+            res.json({
+                message:err.message
+            })
+        })
 
 
 })
