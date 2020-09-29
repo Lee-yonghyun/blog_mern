03eb4dc3ac0140516c5900fc803044bd7a714ccc
이용현 <dydwk231@naver.com>
2020-09-29 14:17:05 +0900

    add post register

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,33 @@ const express = require('express')
 const router = express.Router()
 const postModel = require('../models/post')
 
+const passport = require('passport')
+const checkAuth = passport.authenticate("jwt",{session:false})
+
+
+//@route    POST http://localhost:6001/post/register
+//@desc     Register post
+//@access   Private
+router.post('/register' ,checkAuth, (req,res) => {
+
+    const newPost = new postModel({
+        text : req.body.text,
+        user : req.user.id,
+        name : req.user.name,
+        avatar : req.user.avatar
+    })
+
+    newPost
+        .save()
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message:err.message
+            })
+        })
+})
 
 
 
