ebf8e42c8c758565ffe35d282d50cf3190d92076
이용현 <dydwk231@naver.com>
2020-10-03 14:39:49 +0900

    add like/unlike post

--- routes/post.js
+++ routes/post.js
@@ -131,4 +131,84 @@ router.delete('/:post_id',checkAuth,(req,res) => {
 })
 
 
+
+
+//@route    POST http://localhost:6000/post/like/:post_id
+//@desc     Like post
+//@access   Private
+router.post('/like/:post_id',checkAuth, (req,res) => {
+
+    const id = req.params.post_id
+
+    postModel
+        .findById(id)
+        .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                return res.status(200).json({
+                    message: 'user already liked this post'
+                })
+            }
+            else {
+                post.likes.unshift({user: req.user.id})
+                post
+                    .save()
+                    .then(post => res.status(200).json(post))
+                    .catch(err => {
+                        res.status(400).json({
+                            message: err.message
+                        })
+                    })
+            }
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: err.message
+            })
+        })
+})
+
+
+//@route    DElete http://localhost:6000/post/unlike/:post_id (좋아요 취소하기)
+//@desc     UnLike post
+//@access   Private
+router.delete('/unlike/:post_id',checkAuth,(req,res) => {
+
+    const id = req.params.post_id
+
+    postModel
+        .findById(id)
+        .then(post => {
+            console.log(post)
+            console.log(req.user)
+            if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+                return res.status(400).json({
+                    message: "you have not liked this post"
+                })
+            }
+            else {
+                //get Remove index
+                const removeIndex = post.likes
+                    .map(item => item.user.toString())
+                    .indexOf(req.user.id)
+
+                post.likes.splice(removeIndex, 1)
+
+                post
+                    .save()
+                    .then(post => res.status(200).json(post))
+                    .catch(err => {
+                        res.status(400).json({
+                            message: err.message
+                        })
+                    })
+            }
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: err.message
+            })
+        })
+})
+
+
 module.exports = router
